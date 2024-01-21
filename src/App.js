766bdbdbe0924ee0f6932e19b6ec9bb742fd9441
“Pia <pia.hoelzl@gmx.at>
2024-01-21 20:56:27 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import './index.css';
 import { saveAs } from 'file-saver';
 import React, { useState } from 'react';
 
 export default function MemeGenerator() {
   return (
     <div>
-      <h1>React Meme Generator</h1>
+      <h1>Meme Generator</h1>
       <MemeGeneratorForm />
     </div>
   );
@@ -14,7 +15,7 @@ function MemeGeneratorForm() {
   const [topText, setTopText] = useState('_');
   const [bottomText, setBottomText] = useState('_');
   const [selectedTemplate, setSelectedTemplate] = useState('michael-scott');
-  const generatedMeme = `https://api.memegen.link/images/${selectedTemplate}/${topText}/${bottomText}.png`;
+  const generatedMeme = `https://api.memegen.link/images/${selectedTemplate}/${topText}/${bottomText}.jpg`;
 
   function handleTopTextChange(event) {
     setTopText(event.target.value);
@@ -34,13 +35,13 @@ function MemeGeneratorForm() {
 
   return (
     <div>
-      <label htmlFor="top-text">Top text</label>
+      <label htmlFor="top-text">Top text: </label>
       <input id="top-text" onChange={handleTopTextChange} />
 
-      <label htmlFor="bottom-text">Bottom text</label>
+      <label htmlFor="bottom-text">Bottom text: </label>
       <input id="bottom-text" onChange={handleBottomTextChange} />
 
-      <label htmlFor="meme-template">Meme template</label>
+      <label htmlFor="meme-template">Meme template: </label>
       <input id="meme-template" onChange={handleTemplateChange} />
 
       <div id="meme-image">
@@ -59,7 +60,7 @@ function MemeGeneratorForm() {
         )}
       </div>
 
-      <div className="MemeGenerator">
+      <div id="button" className="MemeGenerator">
         <button onClick={handleClick}>Download</button>
       </div>
     </div>
